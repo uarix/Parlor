215b2a5f2e0a25aab67c3ef90727eae5e927c69c
Quarix <hidden@privacy.privacy>
2024-08-16 22:30:48 +0800

    修改加载动画效果

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,7 +1,34 @@
 <template>
   <div id="loader-wrapper" :class="store.imgLoadStatus ? 'loaded' : null">
     <div class="loader">
-      <div class="loader-circle" />
+      <main>  
+        <svg xmlns="http://www.w3.org/2000/svg" height="128px" width="256px" viewBox="0 0 256 128" class="ip">  
+          <defs>  
+            <linearGradient y2="0" x2="1" y1="0" x1="0" id="grad1">  
+              <stop stop-color="#5ebd3e" offset="0%"></stop>  
+              <stop stop-color="#ffb900" offset="33%"></stop>  
+              <stop stop-color="#f78200" offset="67%"></stop>  
+              <stop stop-color="#e23838" offset="100%"></stop>  
+            </linearGradient>  
+            <linearGradient y2="0" x2="0" y1="0" x1="1" id="grad2">  
+              <stop stop-color="#e23838" offset="0%"></stop>  
+              <stop stop-color="#973999" offset="33%"></stop>  
+              <stop stop-color="#009cdf" offset="67%"></stop>  
+              <stop stop-color="#5ebd3e" offset="100%"></stop>  
+            </linearGradient>  
+          </defs>  
+          <g stroke-width="16" stroke-linecap="round" fill="none">  
+            <g stroke="#ddd" class="ip__track">  
+              <path d="M8,64s0-56,60-56,60,112,120,112,60-56,60-56"></path>  
+              <path d="M248,64s0-56-60-56-60,112-120,112S8,64,8,64"></path>  
+            </g>  
+            <g stroke-dasharray="180 656">  
+              <path d="M8,64s0-56,60-56,60,112,120,112,60-56,60-56" stroke-dashoffset="0" stroke="url(#grad1)" class="ip__worm1"></path>  
+              <path d="M248,64s0-56-60-56-60,112-120,112S8,64,8,64" stroke-dashoffset="358" stroke="url(#grad2)" class="ip__worm2"></path>  
+            </g>  
+          </g>  
+        </svg>  
+      </main>  
       <div class="loader-text">
         <span class="name">
           {{ siteName }}
@@ -23,130 +50,142 @@ const store = mainStore();
 const siteName = import.meta.env.VITE_SITE_NAME;
 </script>
 
-<style lang="scss" scoped>
-#loader-wrapper {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 999;
-  overflow: hidden;
-  .loader {
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    .loader-circle {
-      width: 150px;
-      height: 150px;
-      border-radius: 50%;
-      border: 3px solid transparent;
-      border-top-color: #fff;
-      animation: spin 1.8s linear infinite;
-      z-index: 2;
+<style lang="scss" scoped>  
+#loader-wrapper {  
+  position: fixed;  
+  top: 0;  
+  left: 0;  
+  width: 100%;  
+  height: 100%;  
+  z-index: 999;  
+  overflow: hidden;  
+  display: flex;  
+  flex-direction: column;  
+  align-items: center;  
+  justify-content: center;  
+  background-color: var(--bg);  
+  color: var(--fg);  
 
-      &:before {
-        content: "";
-        position: absolute;
-        top: 5px;
-        left: 5px;
-        right: 5px;
-        bottom: 5px;
-        border-radius: 50%;
-        border: 3px solid transparent;
-        border-top-color: #a4a4a4;
-        animation: spin-reverse 0.6s linear infinite;
-      }
+  main {  
+    padding: 24px 0;  
+  }  
 
-      &:after {
-        content: "";
-        position: absolute;
-        top: 15px;
-        left: 15px;
-        right: 15px;
-        bottom: 15px;
-        border-radius: 50%;
-        border: 3px solid transparent;
-        border-top-color: #d3d3d3;
-        animation: spin 1s linear infinite;
-      }
-    }
-    .loader-text {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      color: #fff;
-      z-index: 2;
-      margin-top: 40px;
-      font-size: 24px;
-      .tip {
-        margin-top: 6px;
-        font-size: 18px;
-        opacity: 0.6;
-      }
-    }
-  }
-  .loader-section {
-    position: fixed;
-    top: 0;
-    width: 51%;
-    height: 100%;
-    background: #333;
-    z-index: 1;
-    &.section-left {
-      left: 0;
-    }
-    &.section-right {
-      right: 0;
-    }
-  }
-  &.loaded {
-    visibility: hidden;
-    transform: translateY(-100%);
-    transition:
-      transform 0.3s 1s ease-out,
-      visibility 0.3s 1s ease-out;
-    .loader {
-      .loader-circle,
-      .loader-text {
-        opacity: 0;
-        transition: opacity 0.3s ease-out;
-      }
-    }
-    .loader-section {
-      &.section-left {
-        transform: translateX(-100%);
-        transition: transform 0.5s 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-      }
-      &.section-right {
-        transform: translateX(100%);
-        transition: transform 0.5s 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-      }
-    }
-  }
-}
+  .ip {  
+    width: 200px;  
+    height: 100px;  
+  }  
 
-@keyframes spin {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
+  .ip__track {  
+    stroke: hsl(var(--hue),90%,90%);  
+    transition: stroke var(--trans-dur);  
+  }  
 
-@keyframes spin-reverse {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(-360deg);
-  }
-}
+  .ip__worm1,  
+  .ip__worm2 {  
+    animation: worm1 2s linear infinite;  
+  }  
+
+  .ip__worm2 {  
+    animation-name: worm2;  
+  }  
+
+  .loader-text {  
+    display: flex;  
+    flex-direction: column;  
+    align-items: center;  
+    color: var(--fg);  
+    z-index: 2;  
+    margin-top: 20px;  
+    font-size: 24px;  
+    .tip {  
+      margin-top: 6px;  
+      font-size: 18px;  
+      opacity: 0.6;  
+    }  
+  }  
+
+  .loader-section {  
+    position: fixed;  
+    top: 0;  
+    width: 51%;  
+    height: 100%;  
+    background: var(--bg);  
+    z-index: 1;  
+    &.section-left {  
+      left: 0;  
+    }  
+    &.section-right {  
+      right: 0;  
+    }  
+  }  
+
+  &.loaded {  
+    visibility: hidden;  
+    transform: translateY(-100%);  
+    transition:  
+      transform 0.3s 1s ease-out,  
+      visibility 0.3s 1s ease-out;  
+    main, .loader-text {  
+      opacity: 0;  
+      transition: opacity 0.3s ease-out;  
+    }  
+    .loader-section {  
+      &.section-left {  
+        transform: translateX(-100%);  
+        transition: transform 0.5s 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);  
+      }  
+      &.section-right {  
+        transform: translateX(100%);  
+        transition: transform 0.5s 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);  
+      }  
+    }  
+  }  
+}  
+
+:root {  
+  --hue: 223;  
+  --bg: hsl(var(--hue),90%,95%);  
+  --fg: hsl(var(--hue),90%,5%);  
+  --trans-dur: 0.3s;  
+}  
+
+@media (prefers-color-scheme: dark) {  
+  :root {  
+    --bg: hsl(var(--hue),90%,5%);  
+    --fg: hsl(var(--hue),90%,95%);  
+  }  
+
+  .ip__track {  
+    stroke: hsl(var(--hue),90%,15%);  
+  }  
+}  
+
+@keyframes worm1 {  
+  from {  
+    stroke-dashoffset: 0;  
+  }  
+  50% {  
+    animation-timing-function: steps(1);  
+    stroke-dashoffset: -358;  
+  }  
+  50.01% {  
+    animation-timing-function: linear;  
+    stroke-dashoffset: 358;  
+  }  
+  to {  
+    stroke-dashoffset: 0;  
+  }  
+}  
+
+@keyframes worm2 {  
+  from {  
+    stroke-dashoffset: 358;  
+  }  
+  50% {  
+    stroke-dashoffset: 0;  
+  }  
+  to {  
+    stroke-dashoffset: -358;  
+  }  
+}  
 </style>
